1eb321cba20ef3714a58814ce467b9c99d92d17c
Zach Niemann <czniemann1990@gmail.com>
2021-01-14 14:55:30 -0600

    Mod: <LocationSearch /> 	- handleSubmit() 		* sends api request for potential location matches, 		  parsing out data needed
    		* updates tempLocation state
    
    	- handleClear()
    		* clears inputfield and tempLocation state on back arrow
    		  click

--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -1,53 +1,65 @@
-import react, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Inputfield from "./Inputfield";
 import useInput from "./hooks/useInput";
+import LocationsList from "./LocationsList";
+import { autoCompleteSearch } from "./getWeatherData";
 
 function LocationSearch(props) {
   const [tempLocations, setTempLocations] = useState(null);
-  const [isContainerFocused, setIsContainerFocused] = useState(false);
   const [inputValue, handleInputChange, updateInputValue] = useInput("");
 
-  useEffect(() => {
-    console.log("useEffect() focus changed");
-    console.log(`active focus: ${document.activeElement.classList}`);
-    const activeClasses = [...document.activeElement.classList];
+  //* clears input and temp locations on arrow click
+  function handleClear(e) {
+    console.log("handleClear()");
+    e.preventDefault();
+    updateInputValue("");
+    setTempLocations(null);
+  }
 
-    //* resets search data if focus is lost
-    if (
-      !(
-        activeClasses.includes("form__input") ||
-        activeClasses.includes("icon-btn")
-      )
-    ) {
-      console.log("reset based on active class");
-      setTempLocations(null);
-      updateInputValue("");
-    }
-  }, [isContainerFocused]);
-
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     console.log("handleSubmit()");
 
     if (inputValue === "") return;
 
-    console.log(`\t - making api request now: ${inputValue}`);
-    //TODO: autocomplete search api request
-    //TODO: response array stored in tempLocations (filter out duplicates)
+    //TODO: don't send api request if input is the same as current location (parent state)
+    const locationsArr = await autoCompleteSearch(inputValue);
+
+    //* stores only the city, state out of the location name
+    let locationsSplit = [];
+    for (let i = 0; i < locationsArr.length; i++) {
+      const tempSplit = locationsArr[i].name.split(",");
+      const tempFinal = tempSplit[0] + "," + tempSplit[1];
+
+      locationsSplit.push({
+        name: tempFinal,
+        id: `${locationsArr[i].id}`,
+        dataCoords: `${locationsArr[i].lat},${locationsArr[i].lon}`,
+      });
+    }
+
+    setTempLocations(locationsSplit);
+  }
+
+  function handleLocationSelect(e) {
+    e.preventDefault();
+    const { id, className } = e.target;
+    console.log(`\n\nhandleLocationSelect() className: ${className}`);
+    console.log(`\tid: ${id}`);
+
+    //TODO: update current location (parent state) if NEW location is selected
+    //TODO: clear out current input and temp locations since one was selected
   }
 
   return (
-    <div
-      className="loc-search"
-      onFocus={() => setIsContainerFocused(true)}
-      onBlur={() => setIsContainerFocused(false)}
-    >
+    <div className="loc-search" onClick={handleLocationSelect}>
       <Inputfield
         value={inputValue}
         handleChange={handleInputChange}
-        handleClear={() => updateInputValue("")}
+        handleClear={handleClear}
         handleSubmit={handleSubmit}
       />
+      <LocationsList list={tempLocations} />
     </div>
   );
 }
