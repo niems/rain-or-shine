abff5db4484a3b0965652494e6c83cb9497d81d5
Zach Niemann <czniemann1990@gmail.com>
2021-01-19 14:57:34 -0600

    Mod: split getForecastData() into two functions 	- getForecastData() now returns promise from the api request
    	- parseForecastData() passed this data, returning an array of
    	  relevant data for each day

--- src/components/getWeatherData.js
+++ src/components/getWeatherData.js
@@ -10,17 +10,12 @@ export function autoCompleteSearch(query) {
   return autoLocations;
 }
 
-//* returns array of days (objects) of forecast data based on query and days (max 10)
-export async function getForecastData(query, days = 10) {
-  const url = `${BASE_URL}/forecast.json?key=${API_KEY}&q=${query}&days=${days}`;
-  console.log(`getForecastData() url: ${url}`);
-  const forecastData = await fetch(url).then((response) => response.json());
-
-  if (!forecastData) return;
-
+//* passed forecast api request data
+//* returns array of day objects
+export function parseForecastData(data) {
   const forecastList = [];
 
-  for (let day of forecastData.forecast.forecastday) {
+  for (let day of data.forecast.forecastday) {
     const iconPath = day.day.condition.icon;
     const iconIndex = iconPath.lastIndexOf("/");
 
@@ -52,25 +47,10 @@ export async function getForecastData(query, days = 10) {
   return forecastList;
 }
 
-// export async function testWeatherData(query) {
-// //TODO: search data displayed as list underneath focused inputfield
-// //TODO: after searching new location
-// //* data pulled based on inputfield location query
-// const data = await autoCompleteSearch(query);
-// //console.log(`search data: ${JSON.stringify(data)}`);
+//* returns array of days (objects) of forecast data based on query and days (max 10)
+export function getForecastData(query, days = 10) {
+  const url = `${BASE_URL}/forecast.json?key=${API_KEY}&q=${query}&days=${days}`;
+  const forecastData = fetch(url).then((response) => response.json());
 
-// //TODO: move all forecast weather code to getForecastData function
-// const selectedDataFromList = data[0];
-// const dataCoordinates = `${selectedDataFromList.lat},${selectedDataFromList.lon}`;
-
-// console.log(
-// `selected data from list: ${JSON.stringify(selectedDataFromList)}`
-// );
-
-// const forecastList = await getForecastData(dataCoordinates, 4);
-// console.log(`forecast length: ${forecastList.length}`);
-
-// for (let i = 0; i < forecastList.length; i++) {
-// console.log(`day: ${JSON.stringify(forecastList[i])}`);
-// }
-// }
+  return forecastData;
+}
